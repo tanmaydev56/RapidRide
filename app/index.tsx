a79d2ce5b9e1c490e61ce007c97cf187d4024ae5
Tanmay <tanmaysharma763@gmail.com>
2024-10-29 13:13:14 +0530

    first commit

---
+++ app/index.tsx
@@ -0,0 +1,15 @@
+// Apply polyfills
+import 'react-native-polyfill';
+
+import { useAuth } from "@clerk/clerk-expo";
+import { Redirect } from "expo-router";
+
+const Page = () => {
+  const { isSignedIn } = useAuth();
+
+  if (isSignedIn) return <Redirect href="/(root)/(tabs)/home" />;
+
+  return <Redirect href="/(auth)/welcome" />;
+};
+
+export default Page;
